ab651d4f2a5b9550919f9ee2662dc1abae493f3c
yifei325325 <yifei1193@163.com>
2015-10-18 09:01:54 +0800

    雇员管理系统

--- src/com/oct/seventeenth/EmpSystem.java
+++ src/com/oct/seventeenth/EmpSystem.java
@@ -7,11 +7,49 @@ public class EmpSystem {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Emp emp1 = new Emp("LS001", "宋江", 1000.23f);
+		Emp emp2 = new Emp("LS002", "卢俊义", 1300.23f);
+		Emp emp3 = new Emp("LS003", "吴用", 1230.23f);
+		Emp emp4 = new Emp("LS004", "林冲", 1890.23f);
+		
+		EmpManagement em = new EmpManagement();
+		em.addEmp(emp1);
+		em.addEmp(emp2);
+		em.addEmp(emp3);
+		em.addEmp(emp4);
+		em.showEmpInfo("LS003");
 
 	}
 
 }
-
+//管理雇员类
+class EmpManagement{
+	private ArrayList al = null;
+	public EmpManagement(){
+		al = new ArrayList();
+	}
+//	新加入一个雇员
+	public void addEmp(Emp emp){
+		al.add(emp);
+	}
+//	显示雇员信息
+	public void showEmpInfo(String empNo){
+		for(int i=0;i<al.size();i++){
+			Emp emp = (Emp)al.get(i);
+			if(emp.getEmpNo().equals(empNo)){
+				System.out.println("该雇员的信息如下：");
+				System.out.println("员工编号："+empNo);
+				System.out.println("员工姓名："+emp.getName());
+				System.out.println("员工工资："+emp.getSal());
+				break;
+			}else{
+				System.out.println("无此记录！");
+			}
+		}
+	}
+}
+//雇员类
 class Emp{
 	private String empNo;
 	private String name;
