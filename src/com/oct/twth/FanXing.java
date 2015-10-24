75fae65d8ef096b95f4f8efb6a8648e2a8bede58
yifei325325 <yifei1193@163.com>
2015-10-24 17:25:05 +0800

    泛型的理解

--- src/com/oct/twth/FanXing.java
+++ src/com/oct/twth/FanXing.java
@@ -31,6 +31,30 @@ public class FanXing {
 
 }
 
+class Cat{
+	private String name;
+	private int age;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public Cat(){
+		System.out.println("Ã¨±»´´½¨");
+	}
+	
+}
+
+
 class Dog{
 	private String name;
 	private int age;
