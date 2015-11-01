597d251a2e2fcb3ba8d3d19aac3ec4051b03d3eb
yifei325325 <yifei1193@163.com>
2015-11-01 18:09:57 +0800

    事件监听的学习2

--- src/com/a/e/TestEvent.java
+++ src/com/a/e/TestEvent.java
@@ -22,14 +22,18 @@ public class TestEvent extends JFrame implements ActionListener{
 		mp = new JPanel();
 		jb1 = new JButton("黑色");
 		jb2 = new JButton("红色");
+		Cat mycat = new Cat();
 		
 		mp.setBackground(Color.WHITE);
 //		注册监听源，由TestEvent这个类的实例去监听
 		jb1.addActionListener(this);
 		jb2.addActionListener(this);
+		jb1.addActionListener(mycat);
+		jb2.addActionListener(mycat);
 //		设置监听消息
 		jb1.setActionCommand("black");
 		jb2.setActionCommand("red");
+		
 		
 		this.add(jb1,BorderLayout.NORTH);
 		this.add(mp);
@@ -47,14 +51,24 @@ public class TestEvent extends JFrame implements ActionListener{
 			mp.setBackground(Color.black);
 		}else if(e.getActionCommand().equals("red")){
 			mp.setBackground(Color.RED);
+		}else{
+			System.out.println("不知道");
 		}
+		
 	}
 }
+class Cat implements ActionListener{
 
-//class MyPanel1 extends JPanel{
-//	public void paint(Graphics g){
-//		super.paint(g);
-//		g.setColor(Color.WHITE);
-//		g.fillRect(0, 0, 400, 300);
-//	}
-//}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getActionCommand().equals("black")){
+			System.out.println("猫也知道你按了黑色按钮");
+		}else if(e.getActionCommand().equals("red")){
+			System.out.println("猫也知道你按了红色按钮");
+			
+		}
+	}
+	
+}
+ 
