b57db4a0bb90785b0766a850a213f627341471ed
yifei325325 <yifei1193@163.com>
2015-11-11 17:21:03 +0800

    字符串的学习

---
+++ src/com/a/h/TalkClient.java
@@ -0,0 +1,32 @@
+/**
+ * java socket±à³Ì Client¶Ë
+ */
+package com.a.h;
+import java.net.*;
+public class TalkClient {
+
+	public static void main(String[] args) {
+		String ss = null;
+		try {
+			ss = InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		System.out.println(ss);
+		String ssArray[] = ss.split("\\.");
+//		String ssArray[] = ss.split("0");
+//		for(int i=0;i<ssArray.length;i++){
+//			System.out.println(ssArray[i]);
+//		}
+		System.out.println(ssArray[3]);
+		
+		String test = "helloworld";
+		String s = test.substring(4,9);
+		System.out.println(s);
+		
+
+	}
+
+}
