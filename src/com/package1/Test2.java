d5eef3abe5800bd48542512839b2dcbeee52a4a5
kenny.li <kenny.li@kenny>
2016-07-29 21:55:07 +0800

    修改了Test2.java

--- src/com/package1/Test2.java
+++ src/com/package1/Test2.java
@@ -10,7 +10,11 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Person p = new Person();
+		Person p = new Person(34,"Kenny");
+		
+		System.out.println(p.getAge());
+		System.out.println(p.getName());
+		
 		p.printStar(5);
 
 	}
@@ -18,10 +22,24 @@ public class Test2 {
 }
 
 class Person {
-	public Person() {
+	private int age;
+	private String name;
+
+	public Person(int age, String name) {
+		this.age = age;
+		this.name = name;
 		System.out.println("构造方法运行中。。。");
 	}
-
+	
+	public int getAge(){
+		return age;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	
 	public void printStar(int n) {
 		for (int i = 0; i < n; i++) {
 			System.out.print("#");
--- src/com/package1/package-info.java
+++ src/com/package1/package-info.java
@@ -11,7 +11,7 @@ package com.package1;
  * Test1.java
  * 		打印三角形
  * Test2.java
- * 
+ * 		学习类的构造方法和成员函数
  * 
  * 
  * 
